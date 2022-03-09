a4d1b080657585a00539c7235bd02f03e5a548bc
SoftLeaderGy <826044698@qq.com>
2022-03-09 09:31:10 +0800

    整合 配置MP

---
+++ blog/src/main/java/com/yang/blog/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.yang.blog.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/03/08/21:26
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.yang.blog.dao")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
