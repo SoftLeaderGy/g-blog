db2d924b6061351d61b57fc5dd91eb4f537493fa
SoftLeaderGy <826044698@qq.com>
2022-03-12 13:46:01 +0800

    解决前后端的配置跨域

---
+++ blog/src/main/java/com/yang/blog/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.yang.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description: 解决前后端跨域问题
+ *  跨域配置
+ * @Author: Guo.Yang
+ * @Date: 2022/03/11/22:14
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","HEAD","POST","PUT","DELETE","OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
