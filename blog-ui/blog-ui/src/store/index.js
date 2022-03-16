0cc26744a9491bd1f9485408b39dca1557b0c46f
SoftLeaderGy <826044698@qq.com>
2022-03-16 09:48:29 +0800

    登陆页面、存储token，存储用户信息实现

--- blog-ui/blog-ui/src/store/index.js
+++ blog-ui/blog-ui/src/store/index.js
@@ -5,11 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: '',
+    userInfo: JSON.parse(sessionStorage.getItem("userinfo"))
   },
   getters: {
+    // 类似于java中的 get()
+    getUser: state => {
+      return state.userInfo
+    }
   },
   mutations: {
-  },
+    // 类似于Java中的 set()
+    SET_TOKEN: (state, token) => {
+      // 赋值给全局变量
+      state.token = token
+      // 将token存储在本地的浏览器的localStorage 中，保证浏览器关闭的时候我们的token还在
+      localStorage.setItem("token",token)
+    },
+
+    SET_USERINFO: (state, userInfo) => {
+      // 赋值给全局变量
+      state.userInfo = userInfo
+      // 将token存储在本地的浏览器的localStorage 中，保证浏览器关闭的时候我们的token还在
+      localStorage.setItem("userInfo",JSON.stringify(userInfo))
+      sessionStorage.setItem("userInfo",userInfo)
+    },
+
+    // 删除之后，要把全局变量和localStorage存的变量
+    REMOVE_INFO: (state) => {
+      state.userInfo = ''
+      state.userInfo = {}
+      localStorage.setItem("userInfo",'')
+      localStorage.setItem("token",'')
+    }
+    },
   actions: {
   },
   modules: {
--- blog-ui/blog-ui/src/views/Login.vue
+++ blog-ui/blog-ui/src/views/Login.vue
@@ -1,16 +1,134 @@
 <template>
   <div>
-    <h1>login</h1>
-
+    <el-container>
+      <el-header>
+        <img src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png"
+             style="height: 60%; margin-top: 10px;">
+      </el-header>
+      <el-main>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      ruleForm: {
+        username: 'root',
+        password: '111111',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'change' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        // 将this预存起来，
+        // 原因：在调用this.$axios 后 在里边的this就指的是返回的结果，而不是vue本身的this
+        const _this = this
+        if (valid) {
+          this.$axios.post("http://localhost:8090/login",this.ruleForm)
+          .then(res =>{
+            // 获取accesstoken
+            const jwt = res.headers['accesstoken']
+            // 获取用户信息
+            const userInfo = res.data.data
+
+            // 把数据共享到全局变量中去
+            // 通过_this.$store调用commit 参数一：store定义的方法 。参数二：传入的参数
+            _this.$store.commit("SET_TOKEN",jwt)
+            _this.$store.commit("SET_USERINFO", JSON.stringify(userInfo))
+
+            // 在全局变量中获取用户信息
+            console.log(JSON.parse(_this.$store.getters.getUser))
+
+            if(res.data.code == 200){
+              _this.$router.push("/blogs")
+            }else {
+              _this.$message.error(res.data.msg)
+            }
+            // console.log(jwt)
+            // console.log(userInfo)
+          })
+          .catch(err=>{
+            debugger
+            _this.$message.error(err)
+            console.log(err)
+            return;
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 }
 </script>
 
 <style scoped>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  /*background-color: #E9EEF3;*/
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+.demo-ruleForm {
+  max-width: 500px;
+  margin: auto;
+}
 </style>
