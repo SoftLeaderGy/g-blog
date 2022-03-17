ad87e17b06308867211e23bd5150da3ed1c1970e
SoftLeaderGy <826044698@qq.com>
2022-03-18 00:05:36 +0800

    项目引入 md文件编辑器，并引入渲染md文件样式

--- blog-ui/blog-ui/src/main.js
+++ blog-ui/blog-ui/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './axios.js' // 请求拦截
 
+// 全局注册
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
 // 引入 element
 import Element from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css"
@@ -12,6 +16,8 @@ import axios from 'axios'
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
+// 使用mavonEditor编辑器
+Vue.use(mavonEditor)
 // 设置全局使用
 Vue.use(Element)
 
